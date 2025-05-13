444b08ff0cc52887ceb39f5067f517eb7be5d1e7
이경근 <107021328+kennywestt@users.noreply.github.com>
2025-05-13 20:44:31 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -0,0 +1,10 @@
+function solution(array, n) {
+    var answer = 0;
+    
+    for (i=0; i<array.length; i++){
+        if(array[i] === n){
+            answer++
+        } 
+    }
+    return answer;
+}
