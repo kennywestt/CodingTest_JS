3ec07aaf824138f629c80c003d263acd420980cc
이경근 <107021328+kennywestt@users.noreply.github.com>
2025-07-01 16:00:41 +0900

    [level 0] Title: 카운트 업, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -1,11 +1,11 @@
 function solution(start_num, end_num) {
     var answer = [];
     
-    for(let i = start_num+1 ; i<end_num; i++){
+    for(let i = start_num ; i<=end_num; i++){
         answer.push(i);
     }
-    answer.push(start_num, end_num);
-    answer.sort((a,b)=>a-b);
+    // answer.push(start_num, end_num);
+    // answer.sort((a,b)=>a-b);
     
     return answer;
 }
