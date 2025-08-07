0cbb8963b91845d24ea8c87135611983d7ce2668
이경근 <107021328+kennywestt@users.noreply.github.com>
2025-08-07 19:36:33 +0900

    [level 0] Title: 개미 군단, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,18 @@
+function solution(hp) {
+    let answer = 0;
+    const bigCount = hp/5; // 병정개미 구하는 계산식
+    
+    if(hp >= 5){
+        answer = Math.floor(bigCount); // +장군개미 개수
+        if (bigCount >= 3) {
+            answer +=  Math.floor((hp % 5) / 3); // +병정개미 개수
+            answer += ((hp % 5) % 3) // +일개미 개수
+        } 
+    } else if(hp >= 3){
+        answer = Math.floor(hp / 3) // +병정개미 개수
+        answer += hp % 3 // 일개미 개수
+    } else {
+        return hp;
+    }
+    return answer;
+}
