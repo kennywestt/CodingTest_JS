baddca8c76bc4b41fa06c083e979a20a2e30a683
이경근 <107021328+kennywestt@users.noreply.github.com>
2025-12-10 05:01:10 +0900

    [level 0] Title: 날짜 비교하기, Time: 2.97 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,12 @@
+function solution(date1, date2) {
+    let answer = 0;
+    console.log(date1, date2)
+    for (let i = 0; i<date1.length; i++){
+        if (date1[i] < date2[i]){
+            return 1
+        } else if (date1[i] > date2[i]) {
+            return 0;
+        }
+    }
+    return answer;
+}
