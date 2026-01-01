52fda9dc4d8924d5de86fec44fedfc2f1df8a6b8
이경근 <107021328+kennywestt@users.noreply.github.com>
2026-01-01 15:37:46 +0900

    [level 0] Title: 1로 만들기, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,16 @@
+function solution(num_list) {
+    let answer = 0;
+    for(let i =0; i<num_list.length; i++){
+        let num = num_list[i]
+        while(num>1){
+            if(num%2==0){
+                num = num/2
+                answer++
+            } else{
+                num = (num-1)/2
+                answer++
+            }
+        }   
+    }
+    return answer;
+}
