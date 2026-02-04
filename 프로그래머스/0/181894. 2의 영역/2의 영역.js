d71b322fd79eb77823d7f1507892e565829898c0
이경근 <107021328+kennywestt@users.noreply.github.com>
2026-02-04 17:01:56 +0900

    [level 0] Title: 2의 영역, Time: 0.37 ms, Memory: 43.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+    const firstIdx = arr.indexOf(2);
+    if (firstIdx === -1) {
+        return [-1];
+    }
+    const lastIdx = arr.lastIndexOf(2);
+    return arr.slice(firstIdx, lastIdx + 1);
+}
